8cf2b5bfa15d087a7994da14f483a81155895857
javierjericm <javierjericm@gmail.com>
2025-08-13 21:30:17 +0800

    Added DB

--- src/main/java/com/ciicc/ict/BooksStoreMain.java
+++ src/main/java/com/ciicc/ict/BooksStoreMain.java
@@ -24,9 +24,9 @@ public class BooksStoreMain extends JFrame{
         setSize(400,400);
         setLocationRelativeTo(null);
 
-//        Books.add(new Books(1,"Java Programming for 3 years old","John Smith", 2021,39.99));
-//        Books.add(new Books(2,"Python Basics","Jane Doe", 2020,29.99));
-//        Books.add(new Books(3,"C++ Essentials","Michael Johnson", 2019,49.99));
+       Books.add(new Books(1,"Java Programming for 3 years old","John Smith", 2021,39.99));
+       Books.add(new Books(2,"Python Basics","Jane Doe", 2020,29.99));
+       Books.add(new Books(3,"C++ Essentials","Michael Johnson", 2019,49.99));
 
         newButton.addActionListener(new ActionListener() {
         @Override
---
+++ src/main/java/com/ciicc/ict/tempCodeRunnerFile.java
@@ -0,0 +1,3 @@
+Books.add(new Books(1,"Java Programming for 3 years old","John Smith", 2021,39.99));
+       Books.add(new Books(2,"Python Basics","Jane Doe", 2020,29.99));
+       Books.add(new Books(3,"C++ Essentials","Michael Johnson", 2019,49.99));
